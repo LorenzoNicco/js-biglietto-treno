bd6f20777a1740121b26bcf54439d8f1dbc14292
LorenzoNicco <lorenzoniccolai91@gmail.com>
2022-12-19 16:04:27 +0100

    Add alert prezzi finali

--- js/script.js
+++ js/script.js
@@ -29,15 +29,30 @@ console.log (typeof pricePerKm);
 console.log ('debug ok');
 
 //3.2 Prezzo per minorenni
-let under18Ticket = (pricePerKm / 100) * 20;
+let under18Discount = (pricePerKm / 100) * 20;
 
-console.log (typeof under18Ticket);
+console.log (typeof under18Discount);
 
 console.log ('debug ok');
 
 //3.3 Prezzo per over 65
-let over65Ticket = (pricePerKm / 100) * 40;
+let over65Discount = (pricePerKm / 100) * 40;
 
-console.log (typeof over65Ticket);
+console.log (typeof over65Discount);
 
 console.log ('debug ok');
+
+//3 Prezzo finale
+let under18Ticket = (pricePerKm - under18Discount).toPrecision(2);
+
+let over65Ticket = (pricePerKm - over65Discount).toPrecision(2);
+
+if (age < 18) {
+    alert(under18Ticket);
+}
+else if (age >= 65) {
+    alert(over65Ticket);
+}
+else {
+    alert(pricePerKm.toPrecision(2));
+}
